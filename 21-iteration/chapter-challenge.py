449e70908dac0ac4d9ac3437537d32355cd1af7a
Mohan Lal <mhnuk2007@gmail.com>
2026-02-21 20:18:51 +0500

    Add solution to challenge

---
+++ 21-iteration/chapter-challenge.py
@@ -0,0 +1,11 @@
+# Challenge: Print fruit name using for loop
+fruits = [
+    'apples',
+    'bananas',
+    'dragon fruit',
+    'mangos',
+    'nectarines',
+    'pears',
+]
+for fruit in fruits:
+    print(fruit)
