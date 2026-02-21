af8b2b1fe55f62e546ed4e74c4ffe9e717505142
Mohan Lal <mhnuk2007@gmail.com>
2026-02-21 23:42:06 +0500

    Add code challege solution

---
+++ 22-strings/chapter-challenge.py
@@ -0,0 +1,7 @@
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+
+miles_float = float(miles)
+kilometers_value = miles_float * 1.609344
+
+print(miles, 'miles is equal to', kilometers_value, 'kilometers')
